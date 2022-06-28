a1e1f716832ab87945b93654af045272b40422e9
Samuel_Elijah <samuel20sam@gmail.com>
2022-06-28 07:33:03 +0530

    Adding valid last name

--- src/com/bridgelabz/regex/RegEx.java
+++ src/com/bridgelabz/regex/RegEx.java
@@ -3,19 +3,27 @@ package com.bridgelabz.regex;
 import java.util.Scanner;
 
 public class RegEx {
-    static String firstName = "^[A-Za-z]{2,}$";
-
+    public boolean firstName(String name)
+    {
+        String firstName = "^[A-Za-z]{2,}$";
+        return name.matches(firstName);
+    }
+    public boolean lastName(String name)
+    {
+        String lastName = "^[A-Za-z]{2,}$";
+        return name.matches(lastName);
+    }
     public static void main(String[] args)
     {
+        RegEx regex = new RegEx();
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter the first name :");
-        String testName = scan.nextLine();
-        if(testName.matches(firstName))
-        {
-            System.out.println("Thi is valid first name.");
-        }
-        else {
-            System.out.println("Not a valid name");
-        }
+        String firstName = scan.nextLine();
+        System.out.println(regex.firstName(firstName));
+
+        System.out.println("Enter the last name :");
+        String lastName = scan.nextLine();
+        System.out.println(regex.lastName(lastName));
     }
 }
